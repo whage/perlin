4e6b38c1fa462862d5a18e0103812dc6242d8f2d
whage <whage@freemail.hu>
2020-04-19 17:17:55 +0200

    Trying to generate a grid of cells with perlin noise. Something is not right, taking a break. With Szilvi.

--- perlin.go
+++ perlin.go
@@ -2,10 +2,9 @@ package perlin
 
 import (
 	"fmt"
+	"math/rand"
+	"strings"
 )
-
-var width = 200
-var height = 200
 
 type Vec2D struct {
 	X, Y float64
@@ -19,28 +18,22 @@ func lerp(a, b, t float64) float64 {
 	return a + (b-a)*t
 }
 
-func CreatePPM() {
-	fmt.Println("P3")
-	fmt.Printf("%d %d\n", width, height)
-	fmt.Println("255")
-
-	tl := Vec2D{0,4}
-	tr := Vec2D{1,-3}
-	bl := Vec2D{-2,2}
-	br := Vec2D{-3,1}
-
+func fillGridCell(tl, tr, bl, br Vec2D, width, height int) [][]int {
 	var min float64
 	var max float64
 
-	pixels := make([][]float64, height)
+	values := make([][]float64, height)
+	greyScalePixels := make([][]int, height)
+
 	for i := 0; i < height; i++ {
-		pixels[i] = make([]float64, width)
+		values[i] = make([]float64, width)
+		greyScalePixels[i] = make([]int, width)
 	}
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			value := Fill(tl, tr, bl, br, width, height, x, y)
-			pixels[x][y] = value
+			value := fillPoint(tl, tr, bl, br, width, height, x, y)
+			values[x][y] = value
 
 			if y == 0 && x == 0 {
 				min, max = value, value
@@ -59,13 +52,65 @@ func CreatePPM() {
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			rgbValue := int((pixels[x][y] - min)*scaleFactor)
-			fmt.Printf("%d %d %d\n", rgbValue, rgbValue, rgbValue)
+			greyScalePixels[x][y] = int((values[x][y] - min)*scaleFactor)
 		}
+	}
+
+	return greyScalePixels
+}
+
+func CreatePPM(width, height int) {
+	fmt.Println("P3")
+	fmt.Printf("%d %d\n", width, height)
+	fmt.Println("255")
+
+	gridWidth := 4
+	gridHeight := 3
+
+	gridNodeVectors := make([][]Vec2D, gridHeight+1)
+
+	for j := 0; j < gridHeight; j++ {
+		gridNodeVectors[j] = make([]Vec2D, gridWidth+1)
+		for i := 0; i < gridWidth; i++ {
+			gridNodeVectors[j][i] = Vec2D{rand.Float64(),rand.Float64()}
+		}
+	}
+
+	pixelValues := make([][]string, height)
+	for j := 0; j < height; j++ {
+		pixelValues[j] = make([]string, width)
+	}
+
+	for y := 0; y < gridHeight-1; y++ {
+		for x := 0; x < gridWidth-1; x++ {
+			tl := gridNodeVectors[y][x]
+			tr := gridNodeVectors[y][x+1]
+			bl := gridNodeVectors[y+1][x]
+			br := gridNodeVectors[y+1][x+1]
+
+			cellWidth := width/gridWidth
+			cellHeight := height/gridHeight
+
+			greyScaleValues := fillGridCell(tl, tr, bl, br, cellWidth, cellHeight)
+			
+			for n := 0; n < cellHeight; n++ {
+				for m := 0; m < cellWidth; m++ {
+					pixelValues[x*cellWidth+m][y*cellHeight+n] = fmt.Sprintf("%d %d %d", greyScaleValues[m][n], greyScaleValues[m][n], greyScaleValues[m][n])
+				}
+			}
+		}
+	}
+
+	for j := 0; j < height; j++ {
+		var row []string
+		for i := 0; i < width; i++ {
+			row = append(row, pixelValues[j][i])
+		}
+		fmt.Println(strings.Join(row, " "))
 	}
 }
 
-func Fill(tl, tr, bl, br Vec2D, width, height, x, y int) float64 {
+func fillPoint(tl, tr, bl, br Vec2D, width, height, x, y int) float64 {
 	v := Vec2D{float64(x), float64(y)}
 	dotA := tl.dot(v)
 	dotB := tr.dot(v)
@@ -78,26 +123,3 @@ func Fill(tl, tr, bl, br Vec2D, width, height, x, y int) float64 {
 	lerped := lerp(lerpTop, lerpBottom, float64(y+1)/float64(height))
 	return lerped
 }
-
-/*
-func Fill(tl, tr, bl, br Vec2D, width, height int) [][]float64 {
-	res := make([][]float64, width*height) // TODO: fix!
-
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			v := Vec2D{float64(width), float64(height)}
-			dotA := tl.dot(v)
-			dotB := tr.dot(v)
-			dotC := bl.dot(v)
-			dotD := br.dot(v)
-
-			lerpTop := lerp(dotA, dotB, float64(width)/float64(x))
-			lerpBottom := lerp(dotC, dotD, float64(width)/float64(x))
-
-			res[x][y] = lerp(lerpTop, lerpBottom, float64(height)/float64(y))
-		}
-	}
-
-	return res
-}
-*/
